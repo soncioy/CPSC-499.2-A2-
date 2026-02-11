19b3d596ba3e0e8ddc65acb5cc72091d353dc0c5
Alielabasery <alih.abasery@gmail.com>
2026-02-11 02:43:44 -0700

    Adding some tests for now.

--- A1Maven-New-Branch/A1Maven-New-Branch/src/main/java/Tests/MethodCall.java
+++ A1Maven-New-Branch/A1Maven-New-Branch/src/main/java/Tests/MethodCall.java
@@ -1,7 +1,16 @@
-class MethodCall {
+//Tests standard calls
+package Tests;
+
+public class MethodCall {
     void test() {
-        System.out.println("Hello"); // Standard chained call
-        foo(1, 2);                   // Simple call
-        this.bar();                  // Explicit receiver
+        System.out.println("Hello");
+
+        foo(1, 2);
+        this.bar();
     }
+
+    // Adding dummy methods to satisfy the compiler
+    // These do nothing, but they exist so Java doesn't crash.
+    void foo(int a, int b) {}
+    void bar() {}
 }
